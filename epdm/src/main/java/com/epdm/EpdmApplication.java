f4bea476bdeb1a6a060ff23d3f291f6b72b37963
EPDM\Administrator <3p_admin@3pchain.co.kr>
2022-12-28 09:59:20 +0900

    최초커밋

---
+++ epdm/src/main/java/com/epdm/EpdmApplication.java
@@ -0,0 +1,13 @@
+package com.epdm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpdmApplication.class, args);
+	}
+
+}
---
+++ epdm/src/test/java/com/epdm/EpdmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epdm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
